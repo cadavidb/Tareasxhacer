eca3efd77e3296d73ecae2953a86cc5a2024053e
brayan cadavid <brayan1cadavid@gmail.com>
2020-12-29 23:31:20 -0500

    doc agregado

---
+++ docs/precache-manifest.faa18ea00457a4c977652195a7df18eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1cc8ef5044ffaa230cd05d4c85a31d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b9a8198b3d596dd3bc6",
+    "url": "/static/css/main.cfb3722e.chunk.css"
+  },
+  {
+    "revision": "01c06f112595229994a2",
+    "url": "/static/js/2.05f9dcec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.05f9dcec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9a8198b3d596dd3bc6",
+    "url": "/static/js/main.285b5e2f.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
